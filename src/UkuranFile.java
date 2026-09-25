16cc6ca7c51a63af095d31d92d3b19868079ad5c
Theresia Angel Simbolon <2581015@unai.edu>
2026-09-25 08:39:12 +0700

    Menambahkan perubahan perbaikan

--- src/UkuranFile.java
+++ src/UkuranFile.java
@@ -5,21 +5,19 @@ public class UkuranFile {
 
         Scanner scanner = new Scanner(System.in);
 
-        //Konstanta untuk konversi dari byte ke kilobyte
-    final double SATU_KB = 1024.0;
+        // Konstanta untuk konversi dari byte ke kilobyte
+        final double SATU_KB = 1024.0;
 
-    //Input nama file
-    System.out.print ("Nama fie     :");
-    String namaFile = scanner.nextLine();
+        // Input nama file
+        System.out.print("Nama file     : ");
+        String namaFile = scanner.nextLine();
 
-    //Input ukuran file dalam byte
-    System.out.print("Ukuran (byte)  :");
+        // Input ukuran file dalam byte
+        System.out.print("Ukuran (byte) : ");
+        long ukuranByte = scanner.nextLong();
 
-    //Percobaan menggunakan int:
-    //int ukuranByte = 322225472;
-    //error:  integer number too large karena 322122572 melebihi batas maksismum int.
-
-        // 1024.0 digunakan agar pembagian menghasilkan nilai double dan pecahan tidak hilang.
+        // 1024.0 digunakan agar pembagian menghasilkan nilai double
+        // sehingga bagian pecahan tidak hilang.
         double ukuranKB = ukuranByte / SATU_KB;
 
         // Konversi dari KB ke MB
@@ -32,7 +30,8 @@ public class UkuranFile {
         // Bagian pecahan akan hilang karena int hanya menyimpan bilangan bulat.
         int ukuranMBBulat = (int) ukuranMB;
 
-        // Menghitung selisih antara ukuran MB asli dengan MB yang sudah dibulatkan.
+        // Menghitung selisih antara ukuran MB asli
+        // dengan MB yang sudah dibulatkan.
         double selisihPembulatan = ukuranMB - ukuranMBBulat;
 
         // Menampilkan hasil
@@ -49,4 +48,4 @@ public class UkuranFile {
 
         scanner.close();
     }
-
+}
